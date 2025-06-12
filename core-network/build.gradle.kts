7c2e91b7e9ef82bf44b3b677b859cedd0defa054
gustavo <gustavo.isaac@gmail.com>
2025-06-12 11:36:25 +0100

    Koin dependency

--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -56,6 +56,8 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
+
+            implementation(libs.koin.core)
         }
 
         desktopMain.dependencies {
