4e2e59552f24fcf2acdcd7f656c969ebb9d99c25
gustavo <gustavo.isaac@gmail.com>
2025-06-17 17:07:34 +0100

    Koin setup

--- composeApp/src/desktopMain/kotlin/com/artemissoftware/tridentgames/main.kt
+++ composeApp/src/desktopMain/kotlin/com/artemissoftware/tridentgames/main.kt
@@ -2,12 +2,14 @@ package com.artemissoftware.tridentgames
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.artemissoftware.tridentgames.di.initKoin
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "TridentGames",
     ) {
+        initKoin()
         App()
     }
 }
--- features/game/presentation/src/commonMain/kotlin/com/artemissoftware/presentation/di/GamePresentationModule.kt
+++ features/game/presentation/src/commonMain/kotlin/com/artemissoftware/presentation/di/GamePresentationModule.kt
@@ -4,6 +4,6 @@ import com.artemissoftware.presentation.GameViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
-fun gamePresentationModule() = module {
+val gamePresentationModule = module {
     viewModelOf(::GameViewModel)
 }
