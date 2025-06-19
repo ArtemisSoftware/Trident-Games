a84d788f3ec0bc1fd51e1f0e3b9ad4b94737f9fa
gustavo <gustavo.isaac@gmail.com>
2025-06-19 12:46:59 +0100

    SearchScreen

---
+++ features/search/presentation/src/commonMain/kotlin/com/artemissoftware/search/presentation/SearchScreen.kt
@@ -0,0 +1,134 @@
+package com.artemissoftware.search.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+internal fun SearchScreen(
+    viewModel: SearchViewModel = koinViewModel<SearchViewModel>(),
+    onClick: (Int) -> Unit,
+    onBackClick:()-> Unit
+) {
+
+    val state = viewModel.state.collectAsStateWithLifecycle().value
+
+    SearchContent(
+        state = state,
+        onQueryChange = {
+            viewModel.updateQuery(it)
+        },
+        onClick = onClick,
+        onBackClick = onBackClick
+    )
+
+}
+
+@Composable
+private fun SearchContent(
+    state: SearchState,
+    onQueryChange: (String) -> Unit,
+    onClick: (Int) -> Unit,
+    onBackClick:()-> Unit
+) {
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+            ) {
+                IconButton(onClick = onBackClick) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                TextField(
+                    value = state.query,
+                    onValueChange = onQueryChange,
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent
+                    ),
+                    placeholder = {
+                        Text("Search here...")
+                    }
+                )
+            }
+        }
+    ) {
+
+        if (state.isLoading) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+        if (state.error.isNotBlank()) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(state.error)
+            }
+        }
+
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(state.games) { item ->
+                AsyncImage(
+                    model = item.imageUrl,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(12.dp
+                        ).background(
+                            color = Color.Transparent,
+                            shape = RoundedCornerShape(12.dp)
+                        )
+                        .clip(RoundedCornerShape(12.dp))
+                        .height(250.dp)
+                        .clickable { onClick(item.id) },
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
+}
