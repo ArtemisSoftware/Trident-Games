5642957e3f36fc6aa45258e15b4a74eda01efc87
gustavo <gustavo.isaac@gmail.com>
2025-06-23 12:40:42 +0100

    DetailsViewModel

--- features/game/presentation/src/commonMain/kotlin/com/artemissoftware/game/presentation/details/DetailsState.kt
+++ features/game/presentation/src/commonMain/kotlin/com/artemissoftware/game/presentation/details/DetailsState.kt
@@ -2,7 +2,7 @@ package com.artemissoftware.game.presentation.details
 
 import com.artemissoftware.game.domain.models.GameDetails
 
-data class DetailsState(
+internal data class DetailsState(
     val isLoading: Boolean = false,
     val error: String = "",
     val data: GameDetails? = null
---
+++ features/game/presentation/src/commonMain/kotlin/com/artemissoftware/game/presentation/details/DetailsViewModel.kt
@@ -0,0 +1,49 @@
+package com.artemissoftware.game.presentation.details
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.artemissoftware.game.domain.repository.GameRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+internal class DetailsViewModel(
+    private val gameRepository: GameRepository
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(DetailsState())
+    val state = _state.asStateFlow()
+
+
+    fun getGameDetails(id: Int) {
+        _state.update {
+            it.copy(isLoading = true)
+        }
+        viewModelScope.launch {
+            val result = gameRepository.getDetails(id)
+
+            result
+                .onSuccess { detail ->
+                    _state.update {
+                        it.copy(isLoading = false, data = detail)
+                    }
+                }
+                .onFailure { error ->
+                    _state.update {
+                        it.copy(isLoading = false, error = error.message.toString())
+                    }
+                }
+        }
+    }
+
+
+//    fun save(id: Int, image: String, name: String) = viewModelScope.launch {
+//        saveGameUseCase.invoke(id, image, name)
+//    }
+//
+//    fun delete(id: Int) = viewModelScope.launch {
+//        deleteUseCase.invoke(id)
+//    }
+
+}
