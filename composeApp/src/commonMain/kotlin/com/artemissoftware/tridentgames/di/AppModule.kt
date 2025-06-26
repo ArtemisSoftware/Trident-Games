1ae346bd313e0607a5c46b4a4a24ffaf7c2c52c9
gustavo <gustavo.isaac@gmail.com>
2025-06-26 15:58:18 +0100

    update on AppModule

--- composeApp/src/commonMain/kotlin/com/artemissoftware/tridentgames/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/artemissoftware/tridentgames/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.artemissoftware.tridentgames.di
 
+import com.artemissoftware.database.di.getDatabaseModule
 import com.artemissoftware.network.di.networkModule
 import com.artemissoftware.game.data.di.gameDataModule
 import com.artemissoftware.game.presentation.di.gamePresentationModule
@@ -12,16 +13,12 @@ fun initKoin(koinApplication: ((KoinApplication) -> Unit)? = null) {
     startKoin {
         koinApplication?.invoke(this)
         modules(
+            getDatabaseModule(),
             networkModule,
             gameDataModule,
             gamePresentationModule,
             searchDataModule,
             searchPresentationModule
-
-//            getCoreDatabaseModule(),
-//            getFavoriteDataModule(),
-//            getFavoriteDomainModule(),
-//            getFavoriteUiModule()
         )
     }
 }
