68ccfbb92fb5043fdd19192115b1c5fa34bbae59
gustavo <gustavo.isaac@gmail.com>
2025-06-27 16:56:17 +0100

    Added method to get context for app di

--- composeApp/src/androidMain/kotlin/com/artemissoftware/tridentgames/TridentGamesApplication.kt
+++ composeApp/src/androidMain/kotlin/com/artemissoftware/tridentgames/TridentGamesApplication.kt
@@ -2,10 +2,17 @@ package com.artemissoftware.tridentgames
 
 import android.app.Application
 import com.artemissoftware.tridentgames.di.initKoin
+import org.koin.dsl.module
 
 class TridentGamesApplication : Application(){
     override fun onCreate() {
         super.onCreate()
-        initKoin()
+        initKoin{
+            it.modules(
+                module {
+                    single { this@TridentGamesApplication.applicationContext }
+                }
+            )
+        }
     }
 }
